d6561d037a9855f48f1a00c9135a975fbb4a4cb6
Adrian <adrian.chirinos@etec.sp.gov.br>
2024-10-29 17:54:13 -0300

    Conexão

---
+++ BoinaFront/Site/form/conexão.php
@@ -0,0 +1,21 @@
+ 
+<?php
+$user="Adm";
+$host="localhost";
+$port="3306";
+$banco=" PIdatabase";
+$senha="";
+try{
+    $conn=new PDO( 'mysql:host='.$host.';port='.$port.';dbname='.$banco,$user,$senha);
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+    echo "ERROR:".$e->getMessage();
+}
+
+
+
+
+?>
+
+
+ 
