4c5e72207bb748eb75e6a94825044bc81f1797a1
Adrian <adrian.chirinos@etec.sp.gov.br>
2024-11-06 16:53:30 -0300

    alterações no Banco

---
+++ Documentação/inserts.sql
@@ -0,0 +1,37 @@
+insert into `produtos`(`idProdutos`,`nomeProd`,`valorProd`,`tipoProd`,`pesoProd`,`horarioProd`,`datadeProd`,`validadeProd`) Values 
+(1,'Fatia de Bolo de Chocolate',5.50,'Fatia','100g','12:30',20240928,20240931) ,
+(2,'Biscoito de Gengibre', 4.00,'Unidade','30g','12:30',20240928,20240931 ),
+(3,'Cookie Tradicional', 4.50, 'Unidade','50g','12:30',20240928,20240931),
+(4,'Fatia de Bolo de Cenoura com Chocolate', 4.95,'Fatia','100g','12:30',20240928,20240931),
+(5,'Fatia de Torta de Limão', 5.50, 'Fatia' , '100g','12:30',20240928,20240931 ),
+(6,'Brownie', 5.00,'Pedaço','100g','12:30',20240928,20240931 ),
+
+(7,'Brigadeiro', 2.50,'Unidade','20g','12:30',20240928,20241005),
+(8,'Beijinho', 2.50,'Unidade','20g','12:30',20240928,20241005),
+
+(9,'Bomba de Chocolate', 6.00,'Unidade','70g','12:30',20240928,20240931),
+(10,'Bomba de Creme', 6.00,'Unidade','70g','12:30',20240928,20240931),
+
+(11,'Bolo de Pote Ninho com Nutella',18.00,'Unidade de Pote','400g','12:30',20240928,2024107),
+(12,'Bolo de Pote Ninho com Morango',18.00,'Unidade de Pote','400g','12:30',20240928,20241007),
+(13,'Bolo de Pote Doce de Leite com Abacaxi',18.00,'Unidade de Pote','400g','12:30',20240928,20241007),
+(14,'Bolo de Pote de Chocolate com Morango',18.00,'Unidade de Pote','400g','12:30',20240928,20241007),
+
+(15,'Bolo de Cenoura com Chocolate',49.50,'Inteiro','1Kg','12:30',20240928,20240931),
+(16,'Bolo de Chocolate',55.00,'Inteiro','1Kg','12:30',20240928,20240931),
+(17,'Torta de limão',55.00,'Inteiro','1Kg','12:30',20240928,20240931),
+(18, 'Água Mineral', 3.00, 'Bebida', '500ml','12:30',20240928,20240931),
+(19, 'Suco de Laranja', 5.40, 'Suco', '500ml', '12:30',20240928,20240931),
+(20,'Suco de Abacaxi com Hortelã', 6.60, 'Suco', '500ml','12:30',20240928,20240931),
+(21,'Suco de Maracujá', 5.40, 'Suco', '500ml', '12:30',20240928,20240931),
+(22,'Suco de Uva', 6.60, 'Suco','500ml','13:30',20240921,20240923), 
+(23,'Pepsi 1,5 lt', 15.50, 'Bebida','1500ml', '' ,20240928,20250921), 
+(24,'Coca-Cola 2L', 15.50, 'Refrigerante','2000ml','',20240928,20250921), 
+(25,'Sprite 2L', 15.50, 'Refrigerante','2000ml','',20240928,20250921), 
+(26,'Pepsi Lata', 5.00, 'Refrigerante','350ml','',20240928,20250921), 
+(27,'Coca-Cola Lata', 5.00, 'Refrigerante','350ml','',20240928,20250921), 
+(28,'Guaraná Lata', 5.00, 'Refrigerante',  '350ml','',20240928,20250921), 
+(29,'Sprite Lata', 5.00, 'Refrigerante',  '350ml','',20240928,20250921), 
+(30,'Milk Shake de Morango', 15.00, 'Milk Shake', '500ml','13:30',20240921,20240923),
+(31,'Milk Shake de Baunilha', 15.00, 'Milk Shake', '500ml','13:30',20240921,20240923),
+(32,'Milk Shake de Chocolate', 15.00, 'Milk Shake', '500ml','13:30',20240921,20240923);
