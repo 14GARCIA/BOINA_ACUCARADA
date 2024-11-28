a581fa743e4c012085db2a20b93c14e04b344cbb
henrique <henrique.araujo62@etec.sp.gov.br>
2024-11-27 21:37:18 -0300

    arrumando links

--- BoinaFront/Site/Header.php
+++ BoinaFront/Site/Header.php
@@ -24,7 +24,7 @@
             </div>
             <div class="nav-list">
                 <ul>
-                    <li class="nav-item"><a href="home.php" class="nav-link">Inicio</a></li>
+                    <li class="nav-item"><a href="home.html" class="nav-link">Inicio</a></li>
                     <li class="nav-item"><a href="produtos.php" class="nav-link">Produtos</a></li>
                     <li class="nav-item"><a href="sobrenos.php" class="nav-link">Sobre nós</a></li>
                     <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
@@ -39,7 +39,7 @@
             </nav>
             <div class="mobile-menu">
                 <ul>
-                <li class="nav-item"><a href="home.php" class="nav-link">Inicio</a></li>
+                <li class="nav-item"><a href="home.html" class="nav-link">Inicio</a></li>
                     <li class="nav-item"><a href="produtos.php" class="nav-link">Produtos</a></li>
                     <li class="nav-item"><a href="sobrenos.php" class="nav-link">Sobre nós</a></li>
                     <li class="nav-item"><a href="login.php" class="nav-link">Login</a></li>
